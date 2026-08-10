ea5cfb436329ce0b525c4f6cca8a58fa1f29b094
Maha Chamarty <srimahalakshmichamarty@gmail.com>
2026-08-10 11:44:48 +0530

    Time: 14 ms (91.38%) | Memory: 42.5 MB (78.33%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean winnerSquareGame(int n) {
-        boolean[] dp = new boolean[n+1];
+        boolean ans[] = new boolean[n+1];
         for(int i = 0; i <= n; i++){
             for(int j = 1; j*j <= i; j++){
-                if(!dp[i-j*j]){
-                    dp[i] = true;
+                if(!ans[i-j*j]){
+                    ans[i] = true;
                     break;
                 }
             }
         }
-        return dp[n];
+        return ans[n];
     }
 }

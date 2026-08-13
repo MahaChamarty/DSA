40ae4f11dd299739a450ab8ed891e419b5a42dce
Maha Chamarty <srimahalakshmichamarty@gmail.com>
2026-08-13 18:34:48 +0530

    Time: 116 ms (34.38%) | Memory: 97.6 MB (98.44%) - LeetSync

---
+++ 2319-longest-substring-of-one-repeating-character/longest-substring-of-one-repeating-character.java
@@ -0,0 +1,64 @@
+class Solution {
+    int[] pref, suff, best, len;
+    char[] left, right;
+
+    public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
+        int n = s.length();
+        pref = new int[4*n];
+        suff = new int[4*n];
+        best = new int[4*n];
+        left = new char[4*n];
+        right = new char[4*n];
+        len = new int[4*n];
+        build(1, 0, n-1, s);
+        int ans[] = new int[queryIndices.length];
+        for(int i = 0; i < queryIndices.length; i++){
+            update(1, 0, n-1, queryIndices[i], queryCharacters.charAt(i));
+            ans[i] = best[1];
+        }
+        return ans;
+    }
+
+    void build(int node, int l, int r, String s){
+        if(l == r){
+            left[node] = right[node] = s.charAt(l);
+            pref[node] = suff[node] = best[node] = 1;
+            len[node] = 1;
+            return;
+        }
+        int mid = (l+r)/2;
+        build(node*2, l, mid, s);
+        build(node*2+1, mid+1, r, s);
+        merge(node, node*2, node*2+1);
+    }
+
+    void update(int node, int l, int r, int idx, char c){
+        if(l == r){
+            left[node] = right[node] = c;
+            pref[node] = suff[node] = best[node] = 1;
+            return;
+        }
+        int mid = (l+r)/2;
+        if(idx <= mid) update(node*2, l, mid, idx, c);
+        else update(node*2+1, mid+1, r, idx, c);
+        merge(node, node*2, node*2+1);
+    }
+
+    void merge(int node, int a, int b){
+        left[node] = left[a];
+        right[node] = right[b];
+        pref[node] = pref[a];
+        suff[node] = suff[b];
+        best[node] = Math.max(best[a], best[b]);
+        len[node] = len[a]+len[b];
+        if(right[a] == left[b]){
+            best[node] = Math.max(best[a], best[b]);
+            best[node] = Math.max(best[node], suff[a]+pref[b]);
+            if(pref[a] == len[a]) pref[node] = len[a]+pref[b];
+            if(suff[b] == len[b]) suff[node] = len[b]+suff[a];
+        }
+        else{
+            best[node] = Math.max(best[a], best[b]);
+        }
+    }
+}
